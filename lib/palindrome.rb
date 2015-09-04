6ba7e00b037ae632cd6588e274b0562995582346
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-04 14:17:50 +0100

    question 2 no recursion

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,6 +1,18 @@
 class PalindromeToTheDome
 
+  attr_reader :filename
+
   def self.call(filename)
+    count = 0
+    File.readlines(filename).each { |line|
+      count += palindrome?(line) ? 1 : 0
+    }
+    count
+  end
+
+  def self.palindrome?(line)
+    line = line.downcase.gsub(/[^a-z0-9]/,'')
+    line == line.reverse
   end
 
 end
